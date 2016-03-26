c9f94837cfb29b8832101d0dbd2f153feb14a03d
alexpudge <afeldman@AIR20150901-2.local>
2016-03-26 16:26:16 -0400

    Barry transition

--- js/script.js
+++ js/script.js
@@ -1,77 +1,33 @@
-var myItem;
-
 $('document').ready(function(){
-	console.log("ready");
+	console.log("page ready");
 
-	$('#chat_scroll').scrollTop(1000000);
+	$('#hamburger').click(function(){
 
-	var input = document.getElementById('input');
-	input.focus();
-	input.select();
+		console.log("click");
 
-	$('#submit').click(function(){
-		console.log("click is working");
+		if( $('#nav_ul').css('display') === 'none'){
+		$('#nav_ul').slideDown("100");
+		} else {
+		$('#nav_ul').slideUp("100", function(){
+			$('#nav_ul').removeAttr("style");})
+		}
+	});
+
+	$('.infinite-slider').unslider({
+		infinite: true,
+		autoplay: true,
+		speed: 1000,
+		delay: 5000
 
 
-
-		myItem = $('#input').val();
-		$('ul').append( '<li class="usertext">' + myItem + '</li>');
-		$('#input').val("");
-
-		$('li').click(function(){
-		console.log("li click is working");
-		$(event.target).remove();
 		});
 
-		$('body').css('background-color' , myItem)
-		$('#input').css('background-color' , myItem)
-		$('#submit').css('background-color' , myItem)
+});		
 
-		$('ul').sortable();
 
-		if(myItem === "hello") {
-			$('ul').append( '<li>' + "What's up?" + '</li>');
-		}
 
-		if(myItem === "not much, you?") {
-			$('ul').append( '<li>' + "Same here dog" + '</li>');
-		}
-
-		if(myItem.toLowerCase() === "take me to facebook" || myItem === "where is everyone?"){
-			window.open("http://www.facebook.com")
-		}
-
-		if(myItem === "where am I?"){
-
-		window.open("https://www.google.com/maps/place/115+W+18th+St,+New+York,+NY+10011/@40.7403679,-73.997841,17z/data=!3m1!4b1!4m2!3m1!1s0x89c259bcc3ea399b:0x8cc7820627311c02")
-		}
-
-		if(myItem.toLowerCase() === "clear" || myItem.toLowerCase() === "cl" ){
-			$('li').remove();
-		}
-
-		if(myItem.toLowerCase() === "hey jack" || myItem.toLowerCase() === "hello jack" ){
-			$('ul').append( '<li>' + "Hey!  What's your name?" + '</li>');
-		}
-
-		$('#chat_scroll').scrollTop(1000000000000);
-
-		return false;
-
-	});
 
 
 
 	
 
-
-
-
-
-
-
-
-
-
-
-});
