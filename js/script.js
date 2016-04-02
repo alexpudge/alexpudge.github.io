c49527a01246f98bb43f5c16e19c719aae69658a
alexpudge <afeldman@AIR20150901-2.local>
2016-04-02 12:36:01 -0400

    final

--- js/script.js
+++ js/script.js
@@ -66,9 +66,7 @@ $('document').ready(function(){
 			break;
 
 			case "is it raining?" :
-			$('#main_1').css('display' , 'none');
-			$('#main_weather').css('display' , 'block');
-			$('li').remove();
+			$('ul').append( '<li id="jack_text">' + "To see the forecast, say" + '<br>' + "[show me the forecast]" + '</li>');
 			break;
 
 			case "you are awesome" :
@@ -84,6 +82,8 @@ $('document').ready(function(){
 			$('#main_map').css('display' , 'block');
 			$('li').remove();
 			break;
+
+		
 
 			case "enough" :
 			$('#main_1').css('display' , 'block');
@@ -107,32 +107,6 @@ $('document').ready(function(){
 			break;
 
 		};
-
-
-		// if(myItem === "hello") {
-		// 	$('ul').append( '<li>' + "What's up?" + '</li>');
-		// }
-
-		// if(myItem === "not much, you?") {
-		// 	$('ul').append( '<li>' + "Same here dog" + '</li>');
-		// }
-
-		// if(myItem.toLowerCase() === "take me to facebook" || myItem === "where is everyone?"){
-		// 	window.open("http://www.facebook.com")
-		// }
-
-		// if(myItem === "where am I?"){
-
-		// window.open("https://www.google.com/maps/place/115+W+18th+St,+New+York,+NY+10011/@40.7403679,-73.997841,17z/data=!3m1!4b1!4m2!3m1!1s0x89c259bcc3ea399b:0x8cc7820627311c02")
-		// }
-
-		// if(myItem.toLowerCase() === "clear" || myItem.toLowerCase() === "cl" ){
-		// 	$('li').remove();
-		// }
-
-		// if(myItem.toLowerCase() === "hey jack" || myItem.toLowerCase() === "hello jack" ){
-		// 	$('ul').append( '<li>' + "Hey!  What's your name?" + '</li>');
-		// }
 
 		$('#chat_scroll').scrollTop(1000000000000);
 
